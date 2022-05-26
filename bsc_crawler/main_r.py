473d17d12d68c11efd01efac07e80066385d9741
pswsm <pauseawesome@protonmail.com>
2022-05-26 13:03:18 +0200

    remove try-except from filter

--- bsc_crawler/main_r.py
+++ bsc_crawler/main_r.py
@@ -10,12 +10,9 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def filter(req, resp, req_record):
     '''If request stattsucode is not 200, return none'''
-    try:
-        if resp.http_headers.get_statuscode() != "200":
-            return None, None
-        return req, resp
-    except AttributeError:
-        print('No headers')
+    if resp.http_headers.get_statuscode() != "200":
+        return None, None
+    return req, resp
 
 
 def get_urls(url_file: str) -> list[str]:
