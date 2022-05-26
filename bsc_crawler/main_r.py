7d3b7a645519203fbd411cbd373223ee653009af
pswsm <pauseawesome@protonmail.com>
2022-05-26 10:30:49 +0200

    Filter WARC

--- bsc_crawler/main_r.py
+++ bsc_crawler/main_r.py
@@ -5,16 +5,27 @@ import requests
 # from ssl import ConnectionResetError
 
 
+def filter(req, resp, req_record):
+    '''If request stattsucode is not 200, return none'''
+    if resp.http_header.get_statuscode() != "200":
+        return None, None
+    return req, resp
+
 def get_urls(url_file: str) -> list[str]:
     '''Reads domains from file and splits them'''
     urls: list[str] = Path(url_file).read_text().split('\n')
     return urls
 
 
+def search_href(html: str) -> str:
+    '''Searches an html text for a tags, and returns the link'''
+    return ''
+
+
 def fetch_html(orig_url: str) -> str:
     '''Requests html from given url.
        Tries https, if timeout falls back to http'''
-    with capture_http(f'{orig_url}.warc.gz'):
+    with capture_http(f'{orig_url}.warc.gz', filter):
         try:
             doc = requests.get('https://' + orig_url, timeout=3)
             result: str = f'{orig_url} is online'
@@ -26,6 +37,7 @@ def fetch_html(orig_url: str) -> str:
                 result: str = f'"{orig_url}" timed out'
         except requests.ConnectionError:
             result: str = f'"{orig_url}" not available'
+    print(result)
     return result
 
 
@@ -36,4 +48,5 @@ def main():
         fetch_html(url)
 
 
-main()
+if __name__ == "__main__":
+    main()
