65943ce8b7d5cedc91b1ab18b39f707b45511b23
Pau Figueras <pau@pswsm.cat>
2022-06-01 16:52:28 +0200

    Crawls subfolders of the domain

--- bsc_crawler/follow_hrefs.py
+++ bsc_crawler/follow_hrefs.py
@@ -1,15 +1,21 @@
 import re, requests
 
-def get_url(url: str) -> str:
+def get_html(url: str) -> str:
     '''Returns the html from a given URL'''
-    html: str = requests.get('https://' + url).text
-    return html
+    try:
+        html: str = requests.get('https://' + url, timeout=3, verify=False).text
+    except:
+        print(f'\"{url}\" is not available')
+    if 'html' in locals():
+        return html
+    return ''
 
 def get_links(html: str) -> set[str]:
     '''Searches an html text for urls in <a>'''
-    links: list[str] = re.findall(r"href=\"([/\w]+)\"", html)
+    links: list[str] = re.findall(r"a href=\"([/\w]+)\"", html)
     return set(links)
 
 if __name__ == '__main__':
-    text: str = get_url('acn.cat')
-    print(get_links(text))
+    text: str = get_html('acn.cat')
+    for link in get_links(text):
+        print(requests.get('https://acn.cat' + link).text, f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
--- bsc_crawler/main.py
+++ bsc_crawler/main.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 from warcio.capture_http import capture_http
 import requests, file_utils
+import follow_hrefs
 # from ssl import ConnectionResetError
 
 # Disables warning for having SSL verification disabled
@@ -23,14 +24,14 @@ def url_is_live(url: str) -> bool:
     '''Check if the url is live'''
     try:
         try:
-            resp = requests.get(f'https://{url}', verify=False, timeout=3).status_code
+            resp: int = requests.get(f'https://{url}', verify=False, timeout=3).status_code
             if resp == 200:
                 return True
         except:
             pass
     except:
         try:
-            resp = requests.get(f'http://{url}', verify=False, timeout=3).status_code
+            resp: int = requests.get(f'http://{url}', verify=False, timeout=3).status_code
             if resp == 200:
                 return True
         except:
@@ -38,22 +39,16 @@ def url_is_live(url: str) -> bool:
     return False
 
 
-def search_href(html: str) -> str:
-    '''Searches an html text for a tags, and returns the link'''
-    # unimplemented
-    return ''
-
-
-def fetch_html(orig_url: str):
+def fetch_html(orig_url: str, folder: str = ''):
     '''Requests html from given url.
        Tries https, if timeout falls back to http'''
     if url_is_live(orig_url):
-        with capture_http(f'warcs/{orig_url}.warc.gz'):
+        with capture_http(f'warcs/{orig_url}{folder.replace("/", "_")}.warc.gz'):
             try:
-                requests.get('https://' + orig_url, timeout=3, verify=False)
-                print(f'"{orig_url}" is online')
-            except (requests.ReadTimeout):
-                print(f'"{orig_url}" timed out.')
+                requests.get(f'https://{orig_url}{folder}', timeout=3, verify=False)
+                print(f'"{orig_url}{folder}" is online')
+            except (requests.ReadTimeout, requests.ConnectionError):
+                print(f'"{orig_url}{folder}" timed out.')
 
 
 def main():
@@ -61,7 +56,14 @@ def main():
     all_urls: list[str] = file_utils.get_urls('domains_cat.txt')
     urls: list[str] = file_utils.trim_url_list(file_utils.last_url_index(all_urls), all_urls)
     for url in urls:
+        done_links: list[str] = []
         fetch_html(url)
+        html: str = follow_hrefs.get_html(url)
+        for link in follow_hrefs.get_links(html):
+            if link not in done_links:
+                fetch_html(url, folder=link)
+                done_links.append(link)
+
 
 
 if __name__ == "__main__":
