3771a0492493a062c30252ec27c81a273367fa2c
Pau Figueras <pau@pswsm.cat>
2022-06-08 13:57:55 +0200

    Fixed branch (again)

--- bsc_crawler/follow_hrefs.py
+++ bsc_crawler/follow_hrefs.py
@@ -1,5 +1,9 @@
 import re, requests
 
+# Disables warning for having SSL verification disabled
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 def get_html(url: str) -> str:
     '''Returns the html from a given URL'''
     try:
@@ -12,11 +16,22 @@ def get_html(url: str) -> str:
 
 def get_links(html: str) -> set[str]:
     '''Searches an html text for urls in <a>'''
+<<<<<<< Updated upstream
     regexp: str = r"a.+href=\"([\w\.]*[/\w]+[\.(cat|com|es|net)]*)\""
     links: list[str] = re.findall(regexp, html)
+=======
+    # regex: str = r"<a.+href=\"((?:[\w]+\.?)?[/\w]+(?:\.cat)?)\".*>"
+    regex: str = r"<a.+href=\"((?:https://|[\w:]+\.|http://)?[\/\w\-]+(?:\.cat)?[\/\w+\-]+)\".*>"
+    links: list[str] = re.findall(regex, html, re.UNICODE)
+>>>>>>> Stashed changes
     return set(links)
 
 if __name__ == '__main__':
-    text: str = get_html('acn.cat')
+    text: str = get_html('esperit.cat')
     for link in get_links(text):
+<<<<<<< Updated upstream
         print(requests.get('https://acn.cat' + link).text, "\n" * 5)
+=======
+        print(link)
+#       print(requests.get(f'https://esperit.cat{link}').text, f"\n\n\n\n\n\n\n\n")
+>>>>>>> Stashed changes
--- bsc_crawler/main.py
+++ bsc_crawler/main.py
@@ -20,35 +20,45 @@ def filter(req, resp, req_record):
     return req, resp
 
 
-def url_is_live(url: str) -> bool:
+def url_is_live(url: str) -> tuple[bool, str]:
     '''Check if the url is live'''
     try:
         try:
             resp: int = requests.get(f'https://{url}', verify=False, timeout=3).status_code
             if resp == 200:
-                return True
+                return True, 'https://'
         except:
             pass
     except:
         try:
             resp: int = requests.get(f'http://{url}', verify=False, timeout=3).status_code
             if resp == 200:
-                return True
+                return True, 'http://'
         except:
             pass
-    return False
+    return False, ''
 
 
-def fetch_html(orig_url: str, folder: str = ''):
+def fetch_html(orig_url: str, folder: str = '', autoprotocol: bool = True):
     '''Requests html from given url.
        Tries https, if timeout falls back to http'''
-    if url_is_live(orig_url):
-        with capture_http(f'warcs/{orig_url}{folder.replace("/", "_")}.warc.gz'):
+    is_live, protocol = url_is_live(orig_url)
+    if autoprotocol:
+        if is_live:
+            with capture_http(f'warcs/{orig_url.replace(":", "_").replace("/", "_")}{folder.replace("/", "_")}.warc.gz'):
+                try:
+                    requests.get(f'{protocol}{orig_url}{folder}', timeout=3, verify=False)
+                    print(f'"{orig_url}{folder}" is online')
+                except (requests.ReadTimeout, requests.ConnectionError):
+                    print(f'"{orig_url}{folder}" timed out.')
+    else:
+        with capture_http(f'warcs/{orig_url.replace(":", "_").replace("/", "_")}{folder.replace("/", "_")}.warc.gz'):
             try:
-                requests.get(f'https://{orig_url}{folder}', timeout=3, verify=False)
+                requests.get(f'{orig_url}{folder}', timeout=3, verify=False)
                 print(f'"{orig_url}{folder}" is online')
             except (requests.ReadTimeout, requests.ConnectionError):
                 print(f'"{orig_url}{folder}" timed out.')
+
 
 
 def main():
@@ -56,13 +66,17 @@ def main():
     all_urls: list[str] = file_utils.get_urls('domains_cat.txt')
     urls: list[str] = file_utils.trim_url_list(file_utils.last_url_index(all_urls), all_urls)
     for url in urls:
-        done_links: list[str] = []
+        done_links: list[str] = [ url ]
         fetch_html(url)
         html: str = follow_hrefs.get_html(url)
         for link in follow_hrefs.get_links(html):
             if link not in done_links:
-                fetch_html(url, folder=link)
-                done_links.append(link)
+                if link.startswith('http'):
+                    fetch_html(link, autoprotocol=False)
+                    done_links.append(link)
+                else:
+                    fetch_html(url, folder=link)
+                    done_links.append(link)
 
 
 
