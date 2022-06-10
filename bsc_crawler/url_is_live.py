39444d78c8304a5ccda8a3d5a7815a3e88f71223
Pau Figueras <pau@pswsm.cat>
2022-06-10 13:11:21 +0200

    Previously removed function moved here

---
+++ bsc_crawler/url_is_live.py
@@ -0,0 +1,27 @@
+'''Checks if url is live and returns the protocol to which it responded'''
+import requests
+
+def url_is_live(url: str) -> tuple[bool, str]:
+    '''Check if the url is live.
+       Returns a tuple, consisting of a bool indicating if the url is live,
+       and a string with the protocol used (either http or https)'''
+    try:
+        try:
+            resp: int = requests.get(f'https://{url}', timeout=3).status_code
+            if resp == 200:
+                return True, 'https://'
+        except:
+            pass
+    except:
+        try:
+            resp: int = requests.get(f'http://{url}', timeout=3).status_code
+            if resp == 200:
+                return True, 'http://'
+        except:
+            pass
+    return False, ''
+
+
+if __name__ == "__main__":
+    liveness, protocol = url_is_live('pswsm.cat')
+    print(f"'pswsm.cat' is live: {liveness}\nProtocol used: {protocol}")
