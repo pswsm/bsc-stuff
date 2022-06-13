8533dd215017906221226ee1adc60ffa8d725efa
Pau Figueras <pau@pswsm.cat>
2022-06-13 12:33:55 +0200

    Prettier code

--- bsc_crawler/file_utils.py
+++ bsc_crawler/file_utils.py
@@ -1,23 +1,27 @@
+'''Searches the las warc done, and compares it to the domain list'''
 from pathlib import Path
 
 def get_urls(url_file: str) -> list[str]:
     '''Reads domains from file and splits them'''
-    urls: list[str] = Path(url_file).read_text().split('\n')
-    return urls
+    url_list: list[str] = Path(url_file).read_text(encoding='utf-8').split('\n')
+    return url_list
 
 
-def last_url_index(urls: list[str]) -> int:
+def last_url_index(url_list: list[str]) -> int:
     '''Compares the warc recors ds with the file to resume from where the process finished'''
-    warcs: list[Path] = [warc for warc in Path('./warcs').glob('*.warc.gz')]
+    warcs: list[Path] = list(Path('./warcs').glob('*.warc.gz'))
     if not warcs:
         return 0
-    indexes: list[int] = [urls.index(Path(warc.stem).stem) for warc in warcs if Path(warc.stem).stem in urls]
+    indexes: list[int] = [url_list.index(Path(warc.stem).stem) for warc in warcs
+                          if Path(warc.stem).stem in url_list]
+    if not indexes:
+        return 0
     return max(indexes)
 
 
-def trim_url_list(idx: int, urls: list[str]) -> list[str]:
+def trim_url_list(idx: int, url_list: list[str]) -> list[str]:
     '''Returns a list starting from index'''
-    return [url for url in urls[idx::]]
+    return list(url_list[idx:])
 
 if __name__ == "__main__":
     urls: list[str] = get_urls("domains_cat.txt")
--- bsc_crawler/warc_utils.py
+++ bsc_crawler/warc_utils.py
@@ -1,27 +1,29 @@
 '''Does everything related to network and warcs'''
-from pathlib import Path
 from warcio.capture_http import capture_http
 import requests
 from url_is_live import url_is_live
 
 
-def filter(req, resp, req_record):
+def filter_w(req, resp, req_record):
     '''If request.statsucode is not 200, do not create a warc file'''
     try:
         if resp.http_headers.get_statuscode() != 200:
             return None, None
     except AttributeError:
-       return None, None 
+        return None, None
+
     return req, resp
 
 
 def fetch_html(orig_url: str, folder: str = '', warc_folder: str = 'warcs'):
     '''Requests html from given url.
        Uses protocol (http or https) from
+
        return value of is_live() function'''
     is_live, protocol = url_is_live(orig_url)
+
     if is_live:
-        with capture_http(f'{warc_folder}/{orig_url.replace("/", "_")}{folder.replace("/", "_")}.warc.gz', filter):
+        with capture_http(f'{warc_folder}/{orig_url.replace("/", "_")}{folder.replace("/", "_")}.warc.gz', filter_w):
             requests.get(f'{protocol}{orig_url}{folder}', timeout=3)
             print(f'"{orig_url}{folder}" is online')
 
@@ -34,6 +36,7 @@ if __name__ == "__main__":
     fetch_html(the_url, warc_folder='tests/warc_utils')
     links: set[str] = remove_prefix(the_url, get_links(html))
     print(links)
+
     for link in links:
         if not link.startswith('/'):
             link = f"/{link}"
