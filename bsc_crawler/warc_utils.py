fa98ce541562857374c8aeff2dfe14eae872f1b5
Pau Figueras <pau@pswsm.cat>
2022-06-14 16:58:43 +0200

    Implement http sessions

--- bsc_crawler/warc_utils.py
+++ bsc_crawler/warc_utils.py
@@ -1,6 +1,7 @@
 '''Does everything related to network and warcs'''
 from warcio.capture_http import capture_http
 import requests
+from requests.models import ReadTimeoutError
 from url_is_live import url_is_live
 
 
@@ -15,30 +16,36 @@ def filter_w(req, resp, req_record):
     return req, resp
 
 
-def fetch_html(orig_url: str, folder: str = '', warc_folder: str = 'warcs'):
+def fetch_html(orig_url: str, https_session: requests.Session, folder: str = '', warc_folder: str = 'warcs'):
     '''Requests html from given url.
        Uses protocol (http or https) from
-
        return value of is_live() function'''
     is_live, protocol = url_is_live(orig_url)
+    savefile: str = folder.replace('/', '_')
+    if len(folder) > 74:
+        savefile = str([word[0] for word in set(folder.replace('/', '_').split('-'))])
 
     if is_live:
-        with capture_http(f'{warc_folder}/{orig_url.replace("/", "_")}{folder.replace("/", "_")}.warc.gz', filter_w):
-            requests.get(f'{protocol}{orig_url}{folder}', timeout=5)
-            print(f'"{orig_url}{folder}" is online')
+        try:
+            with capture_http(f'{warc_folder}/{orig_url.replace("/", "_")}{savefile}.warc.gz', filter_w):
+                https_session.get(f'{protocol}{orig_url}{folder}', timeout=5)
+                print(f'"{protocol}{orig_url}{folder}" is online')
+        except requests.ReadTimeout:
+            print(f"{protocol}{orig_url}{folder} was online, but it's response timed out")
 
 
 if __name__ == "__main__":
     from html_utils import get_html, remove_prefix, get_links
-    the_url: str = 'plataforma-llengua.cat'
+    sess: requests.Session = requests.Session()
+    the_url: str = 'aceb.cat'
     done_links: list[str] = [ the_url ]
     html: str = get_html(the_url)
-    fetch_html(the_url, warc_folder='tests/warc_utils')
+    fetch_html(the_url, sess, warc_folder='tests/warc_utils')
     links: set[str] = remove_prefix(the_url, get_links(html))
-    print(links)
 
     for link in links:
         if not link.startswith('/'):
             link = f"/{link}"
-        fetch_html(the_url, folder=link, warc_folder='tests/warc_utils')
+        print(link)
+        fetch_html(the_url, sess, folder=link, warc_folder='tests/warc_utils')
         done_links.append(link)
