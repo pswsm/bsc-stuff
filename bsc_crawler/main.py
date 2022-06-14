17207231937df3ff7ce45c481724c5a22ce26bcd
Pau Figueras <pau@pswsm.cat>
2022-06-14 16:58:58 +0200

    Remove trailing slashes

--- bsc_crawler/main.py
+++ bsc_crawler/main.py
@@ -2,20 +2,24 @@
 import html_utils
 import warc_utils
 import file_utils
+import requests
 
 def main():
     '''Main function'''
     all_urls: list[str] = file_utils.get_urls('domains_cat.txt')
     urls: list[str] = file_utils.trim_url_list(file_utils.last_url_index(all_urls), all_urls)
+    sess: requests.Session = requests.Session()
     for url in urls:
         done_links: list[str] = [ url ]
-        warc_utils.fetch_html(url)
+        warc_utils.fetch_html(url, sess)
         html: str = html_utils.get_html(url)
         for link in html_utils.remove_prefix(url, html_utils.get_links(html)):
+            if link.endswith('/'):
+                link = link.replace('/', '')
             if not link.startswith("/"):
                 link = f"/{link}"
             if link not in done_links:
-                warc_utils.fetch_html(url, folder=link)
+                warc_utils.fetch_html(url, sess, folder=link)
                 done_links.append(link)
 
 
