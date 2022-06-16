a0fede8750e2784fa1f6114d38a55085f9e0e92f
Pau Figueras <pau@pswsm.cat>
2022-06-16 18:34:04 +0200

    More granular error handling

--- bsc_crawler/url_is_live.py
+++ bsc_crawler/url_is_live.py
@@ -1,28 +1,35 @@
-'''Checks if url is live and returns the protocol to which it responded'''
+'''Checks if url is live and returns the liveness,the protocol and the
+   response code (statuscode) to which it responded'''
 import requests
 
-def url_is_live(url: str) -> tuple[bool, str]:
-    '''Check if the url is live.
+
+def url_is_live(url: str) -> tuple[bool, str, int]:
+    '''Checks if the url is live.
        Returns a tuple, consisting of a bool indicating if the url is live,
        and a string with the protocol used (either http or https)'''
-    resp: int = 0
     try:
         try:
-            resp = requests.get(f'https://{url}', timeout=3).status_code
-            if resp == 200:
-                return True, 'https://'
-        except:
-            pass
-    except:
+            resp = requests.get(f'https://{url}', timeout=3)
+            if resp.status_code == 200:
+                return True, 'https://', resp.status_code
+            raise requests.exceptions.RequestException
+        except requests.exceptions.RequestException as exc:
+            raise requests.exceptions.RequestException from exc
+    except requests.exceptions.RequestException as exc:
         try:
-            resp = requests.get(f'http://{url}', timeout=3).status_code
-            if resp == 200:
-                return True, 'http://'
-        except:
+            resp = requests.get(f'http://{url}', timeout=3)
+            if resp.status_code == 200:
+                return True, 'http://', resp.status_code
+            raise requests.exceptions.RequestException from exc
+        except requests.exceptions.RequestException:
             pass
-    return False, ''
+    return False, '', 404
 
 
 if __name__ == "__main__":
-    liveness, protocol = url_is_live('pswsm.cat')
-    print(f"'pswsm.cat' is live: {liveness}\nProtocol used: {protocol}")
+    is_live, protocol, response = url_is_live('0wx.cat')
+    print(
+        f"'pswsm.cat' is live: {is_live}\nProtocol used: {protocol}\nStatus code: {response}")
+    is_live, protocol, response = url_is_live('1898ramonroqueta.cat')
+    print(
+        f"'fhjfdks.cat' is live: {is_live}\nProtocol used: {protocol}\nStatus code: {response}")
