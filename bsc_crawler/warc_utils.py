2973e7bf9481d9025f0dd12e029c0f529e972088
Pau Figueras <pau@pswsm.cat>
2022-06-23 10:58:50 +0200

    Warcio with requests.sessions does not work

--- bsc_crawler/warc_utils.py
+++ bsc_crawler/warc_utils.py
@@ -6,7 +6,7 @@ import requests
 from url_is_live import url_is_live
 
 
-def fetch_html(orig_url: str, folder: str = "", warc_folder: str = "warcs"):
+def fetch_html(session: requests.Session, orig_url: str, folder: str = "", warc_folder: str = "warcs"):
     """Requests html from given url.
     Uses protocol (http or https) from return value of url_is_live() function"""
     is_live, protocol, _resp = url_is_live(orig_url)
@@ -17,7 +17,7 @@ def fetch_html(orig_url: str, folder: str = "", warc_folder: str = "warcs"):
 
     if is_live:
         try:
-            requests.get(f"{protocol}{orig_url}{folder}", timeout=5)
+            session.get(f"{protocol}{orig_url}{folder}", timeout=5)
         except requests.exceptions.SSLError:
             print(f'"{protocol}{orig_url}{folder}" has an invalid certificate. Skipping')
         except requests.exceptions.RequestException:
@@ -26,7 +26,7 @@ def fetch_html(orig_url: str, folder: str = "", warc_folder: str = "warcs"):
             with capture_http(
                 f'{warc_folder}/{orig_url.replace("/", "_")}{savefile}.warc.gz'
             ):
-                requests.get(f"{protocol}{orig_url}{folder}", timeout=5)
+                session.get(f"{protocol}{orig_url}{folder}", timeout=5)
                 print(f'"{protocol}{orig_url}{folder}" is online')
 
 
@@ -34,13 +34,14 @@ if __name__ == "__main__":
     from html_utils import get_html, get_links, filter_urls
 
     the_urls: list[str] = ['3cat24.cat']
+    the_session: requests.Session = requests.Session()
     for the_url in the_urls:
         html: str = get_html(the_url)
-        fetch_html(the_url, warc_folder="tests/warc_utils")
+        fetch_html(the_session, the_url, warc_folder="tests/warc_utils")
         links: set[str] = filter_urls(the_url, get_links(html))
         for link in links:
             print(link)
             if re.search(r"[\w]\.cat", link, flags=re.UNICODE):
-                fetch_html(link, warc_folder="tests/warc_utils")
+                fetch_html(the_session, link, warc_folder="tests/warc_utils")
             else:
-                fetch_html(the_url, folder=link, warc_folder="tests/warc_utils")
+                fetch_html(the_session, the_url, folder=link, warc_folder="tests/warc_utils")
