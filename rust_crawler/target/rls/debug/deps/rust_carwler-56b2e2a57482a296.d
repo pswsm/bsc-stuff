11b321d35e9183d65e5679b829b788b77088ec64
Pau Figueras <pau@pswsm.cat>
2022-10-06 13:39:19 +0200

    Remove ignored files. Update gitignore

--- rust_crawler/target/rls/debug/deps/rust_carwler-56b2e2a57482a296.d
+++
@@ -1,5 +0,0 @@
-/home/pswsm/code/bsc-stuff/rust_crawler/target/rls/debug/deps/rust_carwler-56b2e2a57482a296.rmeta: src/main.rs
-
-/home/pswsm/code/bsc-stuff/rust_crawler/target/rls/debug/deps/rust_carwler-56b2e2a57482a296.d: src/main.rs
-
-src/main.rs:
--- rust_crawler/target/rls/debug/deps/rust_carwler-e64df878cf67fd37.d
+++
@@ -1,5 +0,0 @@
-/home/pswsm/code/bsc-stuff/rust_crawler/target/rls/debug/deps/rust_carwler-e64df878cf67fd37.rmeta: src/main.rs
-
-/home/pswsm/code/bsc-stuff/rust_crawler/target/rls/debug/deps/rust_carwler-e64df878cf67fd37.d: src/main.rs
-
-src/main.rs:
